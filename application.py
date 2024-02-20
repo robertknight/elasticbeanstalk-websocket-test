e6abc3814e76e738f531e0d272fde2ae171597f8
Robert Knight <robertknight@gmail.com>
2024-02-20 15:00:33 +0000

    Modify the responses as a test

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from websockets.server import serve
 
 async def echo(websocket):
     async for message in websocket:
-        await websocket.send(message)
+        await websocket.send(f"received {message}")
 
 async def main():
     async with serve(echo, "localhost", 8000):
